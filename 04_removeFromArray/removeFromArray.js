99300091b517f272726e77fb012b4103d6d0e12c
Charlize <148724296+charlilize@users.noreply.github.com>
2024-02-07 16:04:04 -0800

    Did Exercises 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    // removeFromArray([1, 2, 2, 3], 2)
 
+    // for every element to remove
+    for (let element of remove) { // 2
+        // search through the entire array
+        for (let j = 0; j < array.length; j++) {
+            if (element === array[j]) {
+                array.splice(j, 1); 
+                j--; // to account for removing an element
+            }
+        }
+    }
+
+    console.log(array);
+
+    return array;
 };
 
 // Do not edit below this line
