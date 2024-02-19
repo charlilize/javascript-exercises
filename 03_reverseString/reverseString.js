bfcf43d346fb9888041c26505f59b667eb229645
Charlize <148724296+charlilize@users.noreply.github.com>
2024-02-19 15:01:59 -0800

    Did JavaScript Exercises and the To-Do List

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function (reverse) {
+    let backwards = "";
+    const letters = [];
 
+    for (let i = 0; i < reverse.length; i++) {
+        letters.push(reverse[i]);
+    }
+
+    for (let j = reverse.length - 1; j >= 0; j--) {
+        backwards += letters[j];
+    }
+
+    return backwards;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secInt) {
+    let largerNum = 0;
+    let smallNum = 0;
 
+    if (firstInt < 0 || secInt < 0 
+        || !(typeof firstInt === 'number') 
+        || !(typeof secInt === 'number')) {
+        return "ERROR"
+    } else if (firstInt > secInt) {
+        largerNum = firstInt;
+        smallNum = secInt;
+    } else if (firstInt < secInt) {
+        largerNum = secInt;
+        smallNum = firstInt;        
+    } 
+
+
+    let sum = 0;
+    for (let i = smallNum; i <= largerNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
