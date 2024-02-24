46ee21979b4a9541965107c73e183cc1ef373970
Charlize <148724296+charlilize@users.noreply.github.com>
2024-02-23 17:31:21 -0800

    Complete 09 Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let arr = string.split("");
+    arr = arr.filter((element) => element != "," &&  element != "!" &&  element != "." && element != " ")
+    let copy = arr.slice().join(""); // keep copy of string without punctuation
+    arr.reverse();
+    arr = arr.join("");
+    // console.log(copy.toLowerCase(), "\n", arr.toLowerCase());
+    return copy.toLowerCase() == arr.toLowerCase();
 };
+
+// convert string to array
+// reverse 
+// compare
 
 // Do not edit below this line
 module.exports = palindromes;
